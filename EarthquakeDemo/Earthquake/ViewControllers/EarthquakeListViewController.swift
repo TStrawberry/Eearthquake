90b65d8442087603c31bba452810b04ac576b3be
Tao Tang <408722734@qq.com>
2024-11-18 10:08:35 +0800

    launch Earthquake project

---
+++ EarthquakeDemo/Earthquake/ViewControllers/EarthquakeListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  EarthquakeDemo
+//
+//  Created by tao on 2024/11/15.
+//
+
+import UIKit
+
+class EarthquakeListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
